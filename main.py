80208883e29ce809e00695408ca53bdd6cadbe8a
joshfink <joshfink@umich.edu>
2023-09-15 15:05:01 -0400

    introduction

--- main.py
+++ main.py
@@ -11,13 +11,23 @@ async def root():
 
 @app.post("/generate_email")
 async def generate_email(
-    name: str=Body(), 
-    email: str=Body(),
+    name: str=Body(),
+    event: str=Body(),
     company: str=Body(),
     role: str=Body(),
     comments: str=Body(),
     api_key: str=Body(),
 ):
     openai.api_key = api_key
-    openai.api_key = ""
-    return "YAY {name} {email} {company} {role} {comments}"
+    
+    introduction = "Dear " + name + ",\nMy name is Joshua Fink, and I just quickly wanted to thank you for our conversation today using my ChatGPT-powered job fair follow up application. (See source code at github.com/joshua-fink/recruitme.)"
+
+
+    #request = 'My name is Joshua Fink, I am a recent University of Michigan computer science graduate looking for work. Write a onb introductory message that I can send on LinkedIn to ' + name.text + ' to see if software engineering or business consulting positions are available. Be sure to emphasize how their experience, especially their current position, would be helpful finding a job. If keyword has abbreviations, do not try to extrapolate what they mean'
+
+    #input = name_string + position_string + most_recent_string + request
+
+    #chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": input}])
+
+
+    return introduction
